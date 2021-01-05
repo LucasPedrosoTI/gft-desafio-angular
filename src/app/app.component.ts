a97f92cd1610e46f3c2f9569d535d5db2b509faf
LucasPedrosoTI <Lucas.PedrosoTI@gmail.com>
2021-01-04 21:33:05 -0300

    login page - parte 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'desafio-I-login';
+export class AppComponent implements OnInit {
+  credentials = {
+    email: '',
+    senha: '',
+  };
+
+  loginForm!: FormGroup;
+
+  onSubmit() {
+    console.log(this.senha);
+  }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm() {
+    this.loginForm = new FormGroup({
+      email: new FormControl(this.credentials.email, [
+        Validators.required,
+        Validators.email,
+      ]),
+      senha: new FormControl(this.credentials.senha, [Validators.required]),
+    });
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get senha() {
+    return this.loginForm.get('senha');
+  }
 }
